3859b475b683f52c5fdd77837d8e06f47e030cd8
tnnd <37441897+tnnd@users.noreply.github.com>
2021-03-16 13:49:08 +0800

    Update rdp.sh
    debug

--- rdp.sh
+++ rdp.sh
@@ -4,18 +4,17 @@ printf "Installing RDP Be Patience... " >&2
 sudo useradd -m colab
 sudo adduser colab sudo
 echo 'colab:8341' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 xrdp nano terminator
+apt install --assume-yes xfce4 xrdp nano terminator iproute2
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo systemctl disable lightdm.service
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo dpkg --install google-chrome-stable_current_amd64.deb
 #sudo apt install --assume-yes --fix-broken
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
 sudo adduser colab chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
@@ -24,9 +23,3 @@ printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debia
 read -p "Paste Here: " CRP
 su - colab -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
