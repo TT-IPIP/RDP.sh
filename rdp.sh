14e4e10f4fed3310bf76ef36223aa01ca5e22ab7
tnnd <37441897+tnnd@users.noreply.github.com>
2021-03-16 13:30:26 +0800

    Update rdp.sh
    debug

--- rdp.sh
+++ rdp.sh
@@ -13,9 +13,9 @@ sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 xrdp nano terminator
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg --install google-chrome-stable_current_amd64.deb
+#sudo apt install --assume-yes --fix-broken
 sudo adduser colab chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
