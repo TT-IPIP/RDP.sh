7d3022df026accef777d7754c9c67082a215f69d
tnnd <37441897+tnnd@users.noreply.github.com>
2021-03-16 13:14:25 +0800

    Update rdp.sh
    DEBUG

--- rdp.sh
+++ rdp.sh
@@ -10,12 +10,14 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 nano
+apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
 sudo adduser N3LK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
